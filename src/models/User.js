c28497a2b9e2db4c28cc93e6808b9025b6c131fe
alexyoliveira <cupofteaoficial@gmail.com>
2023-04-06 18:10:43 -0300

    req 2 done, model User created

---
+++ src/models/User.js
@@ -0,0 +1,19 @@
+ /**
+  * 
+  * @param {import('sequelize').Sequelize} sequelize 
+  * @param {*} DataTypes 
+  */
+
+module.exports = (sequelize, DataTypes) => {
+  const UsersTable = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  },{
+    tableName: 'users',
+    underscored: true,
+  })
+  return UsersTable;
+};
